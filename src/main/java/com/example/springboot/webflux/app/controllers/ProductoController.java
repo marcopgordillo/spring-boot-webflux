8ff09e3fbf9251d46b4a4f8a06dd94518e8082f1
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2019-02-04 23:37:43 -0500

    Modo Reactive Data Driven para manejar la contra presion

--- src/main/java/com/example/springboot/webflux/app/controllers/ProductoController.java
+++ src/main/java/com/example/springboot/webflux/app/controllers/ProductoController.java
@@ -7,7 +7,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
 import reactor.core.publisher.Flux;
+
+import java.time.Duration;
 
 @Controller
 public class ProductoController {
@@ -31,6 +34,23 @@ public class ProductoController {
     productos.subscribe(producto -> logger.info(producto.getNombre()));
 
     model.addAttribute("productos", productos);
+    model.addAttribute("titulo", "Listado de productos");
+
+    return "listar";
+  }
+
+  @GetMapping("/listar-datadriver")
+  public String listarDataDriver(Model model) {
+    Flux<Producto> productos = productoDao.findAll()
+            .map(producto -> {
+              producto.setNombre(producto.getNombre().toUpperCase());
+              return producto;
+            })
+            .delayElements(Duration.ofSeconds(1));
+
+    productos.subscribe(producto -> logger.info(producto.getNombre()));
+
+    model.addAttribute("productos", new ReactiveDataDriverContextVariable(productos, 2));
     model.addAttribute("titulo", "Listado de productos");
 
     return "listar";
